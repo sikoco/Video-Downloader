89f611c30e1a01d22a87c209e5c16334738555b7
Simon Koch <simon.leo.koch@tum.de>
2023-04-19 18:58:17 +0200

    Add parameter for outputfile

--- download.sh
+++ download.sh
@@ -3,11 +3,10 @@
 IFS='/' read -r -a video <<< "$1"
 echo ${video[7]}
 
-if [ "${video[7]}" = "" ]; then
-    output_file="output.mp4" # Set a default output file name
-else
+if [ -z "$2" ]; then
     output_file="${video[7]}"
+else
+    output_file="$2"
 fi
 
 ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "$1" -c copy "$output_file"
-
