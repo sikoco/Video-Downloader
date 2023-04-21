964ff6853e4a43be8ab65b08824852838764c2f2
Simon Koch <simon.leo.koch@tum.de>
2023-04-21 11:00:55 +0200

    Increase time precision in default ouput file name

--- download.sh
+++ download.sh
@@ -8,7 +8,7 @@ Downloads and converts a video file to MP4 format using FFmpeg.
 
 Positional arguments:
   input_file      Input video URL.
-  output_file     (Optional) Output file name. If not provided, defaults to the current date and time in the format YYYY_MM_DD-hh_mm.mp4.
+  output_file     (Optional) Output file name. If not provided, defaults to the current date and time in the format YYYY_MM_DD-hh_mm_ss.mp4.
 
 Optional arguments:
   -h, --help      Show this help message and exit.
@@ -34,7 +34,7 @@ IFS='/' read -r -a video <<< "$1"
 echo ${video[7]}
 
 if [ -z "$2" ]; then
-  output_file=$(date +'%Y_%m_%d-%H_%M').mp4
+  output_file=$(date +'%Y_%m_%d-%H_%M_%S').mp4
 else
   output_file="$2"
 fi
